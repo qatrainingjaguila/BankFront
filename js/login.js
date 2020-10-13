24b4784b7c92e94d5ccb11d03357938e35f11741
qatrainingjaguila <jaguila@academytrainee.com>
2020-10-13 17:46:50 +0100

    Login partially complete

--- js/login.js
+++ js/login.js
@@ -1,29 +1,5 @@
-const createForm = document.getElementById("createForm");
 const loginForm = document.getElementById("loginForm");
-const accountOutput = document.getElementById("accountDiv");
 const loginDiv = document.getElementById("loginDiv");
-
-createForm.addEventListener('submit', function (event) {
-    event.preventDefault();
-    const data = {
-        firstName: this.firstName.value,
-        lastName: this.lastName.value,
-        balance: this.balance.value,
-        pass:this.pass.value
-    }
-
-    fetch("http://localhost:8080/account/createAccount", { //Make request
-        method: "POST",
-        body: JSON.stringify(data),
-        headers: {
-            'Content-Type': "application/json"
-        }
-    }).then(response => { // Receive response
-        return response.json(); // Convert response body to json
-    }).then(data => { //json data from previous .then()
-        this.reset();        
-    }).catch(error => console.log(error));
-});
 
 loginForm.addEventListener('submit', function (event) {
     event.preventDefault();
