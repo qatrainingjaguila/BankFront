c7fb4a2caa6ea1bed7dbea5c00160154797f3a04
qatrainingjaguila <jaguila@academytrainee.com>
2020-10-13 12:10:38 +0100

    first commit

---
+++ js/index.js
@@ -0,0 +1,24 @@
+const createForm = document.getElementById("createForm");
+
+createForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+    console.log(this.name);
+    const data = {
+        firstName: this.firstName.value,
+        lastName: this.lastName.value,
+        balance: this.balance.value
+    }
+
+    fetch("http://localhost:8080/account/createAccount", { //Make request
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+            'Content-Type': "application/json"
+        }
+    }).then(response => { // Receive response
+        return response.json(); // Convert response body to json
+    }).then(data => { //json data from previous .then()
+        //renderDucks();
+        this.reset();
+    }).catch(error => console.log(error));
+});
